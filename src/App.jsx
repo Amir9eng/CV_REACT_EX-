479debc1c7fe9ce113b202cd964527827ba1d27d
Amir9eng <amirmukhtarme@gmail.com>
2020-09-28 16:29:29 +0100

    code-cleanup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './App.scss';
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+function App() {
+ 
+  
+    return (
+      <div>
+       <h1>Hello World</h1>
+      </div>
+    )
+  }
+
+
+export default App;
