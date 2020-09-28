8b8204f2854a93ed1059261b9f49c2697f7b996d
Amir9eng <amirmukhtarme@gmail.com>
2020-09-28 18:35:29 +0100

    feat: addphonebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
 
+import React, { useState } from 'react'
 
-import './App.scss';
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+export const generateId = () => Math.random().toString(36).substring(2, 6)
 
-function App() {
- 
-  
-    return (
-      <div>
-       <h1>Hello World</h1>
-      </div>
-    )
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addNewName = (e) => {
+    e.preventDefault();
+    const newName = {}
+    newInput[generateId()] = {
+      name: newName
+    }
+    setPersons({
+      ...persons,
+      ...newInput
+    })
+      setNewName('')
   }
 
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={e => addNewName(e)}
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <p>
+      { persons.map(person => <span>{person.name}</span>)}
+      </p>
+    </div>
+  )
+}
 
-export default App;
+export default App
+
