8f7b13dcf4f547a0f1039338b3da8cdb5559c23d
Amir9eng <amirmukhtarme@gmail.com>
2020-09-29 12:03:39 +0100

    feat: addnames

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,36 @@ import React, { useState } from 'react'
 export const generateId = () => Math.random().toString(36).substring(2, 6)
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: generateId() }
+  ])
+  const [newName, setNewName] = useState('')
 
   const addNewName = (e) => {
     e.preventDefault();
-    const newName = {}
-    newInput[generateId()] = {
+    const newInput = {}
+    setPersons(persons => persons.concat({
+      id: generateId(),
       name: newName
-    }
-    setPersons({
-      ...persons,
-      ...newInput
-    })
-      setNewName('')
+    }))
+    setNewName('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={e => addNewName(e)}
+      <form onSubmit={e => addNewName(e)}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <p>
-      { persons.map(person => <span>{person.name}</span>)}
-      </p>
+      <p>{
+       persons.map(person => <p key={person.id}>{person.name}</p>)
+      } </p>
     </div>
   )
 }
