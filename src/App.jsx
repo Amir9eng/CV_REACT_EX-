725859e5d4fdd1dd5133c92358376b165bf00946
Amir9eng <amirmukhtarme@gmail.com>
2020-09-29 16:46:06 +0100

    feat: step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-
 import React, { useState } from 'react'
+import './App.css';
+
 
 export const generateId = () => Math.random().toString(36).substring(2, 6)
 
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: generateId() }
+    { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
 
   const addNewName = (e) => {
     e.preventDefault();
+
+    if (Object.keys(persons).some(personId => {
+      const person = persons[personId]
+      return person.name.toLowerCase() === newName.trim().toLowerCase()
+    })) {
+      setNewName('')
+      return window.alert(`${newName} is already added to phonebook`)
+
+    }
+
     const newInput = {}
-    setPersons(persons => persons.concat({
-      id: generateId(),
+    newInput[generateId()] = {
       name: newName
-    }))
+    }
+    setPersons({
+      ...persons,
+      ...newInput
+
+    })
     setNewName('')
   }
-
   return (
     <div>
       <h2>Phonebook</h2>
@@ -32,8 +47,14 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <p>{
-       persons.map(person => <p key={person.id}>{person.name}</p>)
-      } </p>
+        Object.keys(persons).map(
+          personId => {
+            const person = persons[personId]
+            return <p key={personId}>{person.name}</p>
+          }
+        )
+      }
+      </p>
     </div>
   )
 }
