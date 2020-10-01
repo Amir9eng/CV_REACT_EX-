51f48990fbdbea444f5b052c764cbf5a0b8cf62f
Amir9eng <amirmukhtarme@gmail.com>
2020-10-01 18:27:47 +0100

    feat: search-filter

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ const App = () => {
     { id: 3, name: 'Dan Abramov', phoneNumber: '217-16-660040' },
     { id: 4, name: 'Mary Poppins', phoneNumber: '616-39-374784' }
   ])
+  const [query, setQuery] = useState("")
+
+  const personToShow = query.trim() ? persons.filter(person => person.name.toLowerCase().indexOf(query.toLowerCase().trim()) > -1) : persons
+
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
@@ -44,26 +48,41 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <input id="search" type="text" placeholder="Search Contact" value={query} onChange={(e) => setQuery(e.target.value)} />
       <form onSubmit={e => addNewContact(e)}>
         <div>
-          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+          <label htmlFor="name">Name: </label>
+          <input
+            id='name' type="text"
+            placeholder="Add Name"
+            value={newName} name="name"
+            onChange={e => setNewName(e.target.value)}
+          />
         </div>
         <div>
-          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
+          <label htmlFor="number">Number: </label>
+        {/* number:  <input value={newNumber} type="number" id="number" onChange={e => setNewNumber(e.target.value)} /> */}
+          < input
+            id='number' type="number"
+            placeholder="Add Number"
+            value={newNumber} number="number"
+            onChange={e => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <p>{
-        persons.map(person => <p key={person.id}><span>{person.name}</span> &nbsp; <span>{person.phoneNumber}</span>
-        </p>)
+      {
+
+        personToShow.map(person => (<p key={person.id}><span>{person.name} &nbsp; </span><span>{person.phoneNumber}</span></p>))
       }
-      </p>
     </div>
   )
 }
 
 export default App
+
 
